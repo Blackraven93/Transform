ebc13a3c19c1a9e4b11fb0fa8dcd2a5b786708e8
Blackraven93 <reblackraven@gmail.com>
2022-10-01 00:19:27 +0900

    Initial Commit

---
+++ Transform/TransformApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransformApp.swift
+//  Transform
+//
+//  Created by blackRaven on 2022/10/01.
+//
+
+import SwiftUI
+
+@main
+struct TransformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
