9188b8c1c9e2ada814f6ce1126800b62de6013f4
jsh9611 <jangsh9611@naver.com>
2022-10-29 20:08:43 +0900

    [ADD] color assets & fonts

---
+++ Transform/Font/FontManager.swift
@@ -0,0 +1,15 @@
+//
+//  FontManager.swift
+//  Transform
+//
+//  Created by SeongHoon Jang on 2022/10/29.
+//
+
+import SwiftUI
+
+class FontManager {
+    struct Poppins {
+        static let semibold = "Poppins-SemiBold"
+        static let medium = "Poppins-Medium"
+    }
+}
--- Transform/Views/Main/MainView.swift
+++ Transform/Views/Main/MainView.swift
@@ -18,65 +18,71 @@ struct MainView: View {
     @AppStorage("log_status") var log_Status = false
     
     var body: some View {
-        NavigationView{
-            VStack(spacing: 20){
-                Picker("", selection: $segmentationSelection) {
-                    ForEach(MenuList.allCases, id: \.self) { option in
-                        Text(option.rawValue)
+        NavigationView {
+            ZStack {
+                Color("background")
+                    .ignoresSafeArea(.all)
+                
+                VStack(spacing: 20){
+                    Picker("", selection: $segmentationSelection) {
+                        ForEach(MenuList.allCases, id: \.self) { option in
+                            Text(option.rawValue)
+                        }
                     }
-                }
-                .pickerStyle(SegmentedPickerStyle())
-                .padding(.horizontal, 20)
-                .padding(.vertical, 10)
-                
-                switch segmentationSelection {
-                case .todo:
-                    MainTodoView()
-                case .check:
-                    MainCheckView()
-                }
-                
-                Spacer()
-                
-                Button(action: {
-                    // Logging Out User.
-                    DispatchQueue.global(qos: .background).async {
-                        try? Auth.auth().signOut()
-                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 10)
                     
-                    // Setting Back View To Login.
-                    withAnimation(.easeOut){
-                        log_Status = false
+                    switch segmentationSelection {
+                    case .todo:
+                        MainTodoView()
+                    case .check:
+                        MainCheckView()
                     }
+
+                    Spacer()
                     
-                }, label: {
-                    Text("Log Out")
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                        .padding(.vertical,12)
-                        .frame(width: UIScreen.main.bounds.width / 2)
-                        .background(Color.pink)
-                        .clipShape(Capsule())
-                })
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Text("Servay")
-                        .font(.title)
-                        .accessibilityAddTraits(.isHeader)
-                }
-                
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
+                    Button(action: {
+                        // Logging Out User.
+                        DispatchQueue.global(qos: .background).async {
+                            try? Auth.auth().signOut()
+                        }
                         
-                    } label: {
-                        NavigationLink(destination: EmptyView()) {
-                            Image(systemName: "gearshape")
-                                .foregroundColor(.black)
+                        // Setting Back View To Login.
+                        withAnimation(.easeOut){
+                            log_Status = false
+                        }
+                        
+                    }, label: {
+                        Text("Log Out")
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .padding(.vertical,12)
+                            .frame(width: UIScreen.main.bounds.width / 2)
+                            .background(Color.pink)
+                            .clipShape(Capsule())
+                    })
+                }
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Text("Servay")
+                            .font(.title)
+                            .accessibilityAddTraits(.isHeader)
+                    }
+                    
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            
+                        } label: {
+                            NavigationLink(destination: EmptyView()) {
+                                Image(systemName: "gearshape")
+                                    .foregroundColor(.black)
+                            }
                         }
                     }
                 }
             }
+            
         }
     }
 }
