f3440fe8cd1d5971f5c51814a147850c91b954b9
Blackraven93 <reblackraven@gmail.com>
2022-11-03 00:10:35 +0900

    [STYLE] MainView의 TabBarButton들이 묶인 HStack과 하위 버튼이 선택될 때 나타나는 View들의 사이 여백을 제거했습니다

--- Transform/Views/Main/MainView.swift
+++ Transform/Views/Main/MainView.swift
@@ -23,7 +23,7 @@ struct MainView: View {
             ZStack {
                 Color("background").edgesIgnoringSafeArea(.all)
                 
-                VStack(spacing: 20){
+                VStack(spacing: 0){
                     HStack(spacing: 0){
                         TabBarButton(current: $selectedTab, item: "To Do", animation: animation)
                         TabBarButton(current: $selectedTab, item: "Made", animation: animation)
