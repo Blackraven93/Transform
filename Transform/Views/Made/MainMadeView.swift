7ba48687776024f9dd3bd9505b753154637a4185
Blackraven93 <reblackraven@gmail.com>
2022-11-03 00:02:47 +0900

    [FEAT] MainView TabBarButton에 Made 클릭 시 보여지는 하위 View를 작업했습니다

--- Transform/Views/Made/MainMadeView.swift
+++ Transform/Views/Made/MainMadeView.swift
@@ -11,7 +11,50 @@ import SwiftUI
 
 struct MainMadeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Raven")
+                    .fontWeight(.semibold)
+                Spacer()
+                HStack {
+                    Image(systemName: "person.fill")
+                    Text("3/12")
+                        
+                }
+            }.padding(.top, 20)
+            
+            Spacer()
+            
+            Text("온보딩 결과 설문조사 입니다. 확인 부탁드립니다.")
+                .font(.system(size: 30))
+                .lineLimit(2)
+                .multilineTextAlignment(.leading)
+                .fixedSize(horizontal: false, vertical: true)
+                
+                
+            Spacer()
+            
+            HStack {
+                Button("결과 확인") {
+                    
+                }
+                .frame(width: 166, height: 50)
+                .background(Color.accentBlack)
+                .foregroundColor(Color.accentYellow)
+                .cornerRadius(14)
+                Spacer()
+                Button("수정하기") {
+                    
+                }
+                .frame(width: 166, height: 50)
+                .background(Color.accentBlack)
+                .foregroundColor(Color.accentYellow)
+                .cornerRadius(14)
+            }.padding(.bottom, 36)
+        }
+        .frame(height: 317)
+        .padding(.horizontal, 20)
+        .background(Color.accentYellow)
     }
 }
 
